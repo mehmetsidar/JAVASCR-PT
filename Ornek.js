2dfa15b76706770ca5e458c106ed78d083c1da21
Test Kullanıcısı <Test@gmail.com>
2024-05-22 11:34:28 +0300

    Sitemiz Hazırıdır

--- Ornek.js
+++ Ornek.js
@@ -1,15 +1,22 @@
+// "search-form", "cart-items-container" ve "navbar" sınıfına sahip öğeleri seç
 const searchForm = document.querySelector(".search-form");
 const cartItem = document.querySelector(".cart-items-container");
 const navbar = document.querySelector(".navbar");
 
+// "search-btn", "cart-btn" ve "menu-btn" kimliğine sahip öğeleri seç
 const searchBtn = document.querySelector("#search-btn");
 const cartBtn = document.querySelector("#cart-btn");
 const menuBtn = document.querySelector("#menu-btn");
 
-//!Buttons
+//! Butonlar
+// Arama butonuna tıklama olayı dinleyicisi ekle
 searchBtn.addEventListener("click", function () {
+  // Arama butonuna tıklandığında arama formuna "active" sınıfını ekle veya kaldır
   searchForm.classList.toggle("active");
+
+  // Belgeye tıklama olayı dinleyicisi ekle
   document.addEventListener("click", function (e) {
+    // Eğer tıklanan öğe arama butonu veya arama formu değilse, arama formundan "active" sınıfını kaldır
     if (
       !e.composedPath().includes(searchBtn) &&
       !e.composedPath().includes(searchForm)
@@ -19,9 +26,14 @@ searchBtn.addEventListener("click", function () {
   });
 });
 
+// Sepet butonuna tıklama olayı dinleyicisi ekle
 cartBtn.addEventListener("click", function () {
+  // Sepet butonuna tıklandığında sepet öğesine "active" sınıfını ekle veya kaldır
   cartItem.classList.toggle("active");
+
+  // Belgeye tıklama olayı dinleyicisi ekle
   document.addEventListener("click", function (e) {
+    // Eğer tıklanan öğe sepet butonu veya sepet öğesi değilse, sepet öğesinden "active" sınıfını kaldır
     if (
       !e.composedPath().includes(cartBtn) &&
       !e.composedPath().includes(cartItem)
@@ -31,9 +43,14 @@ cartBtn.addEventListener("click", function () {
   });
 });
 
+// Menü butonuna tıklama olayı dinleyicisi ekle
 menuBtn.addEventListener("click", function () {
+  // Menü butonuna tıklandığında menü öğesine "active" sınıfını ekle veya kaldır
   navbar.classList.toggle("active");
+
+  // Belgeye tıklama olayı dinleyicisi ekle
   document.addEventListener("click", function (e) {
+    // Eğer tıklanan öğe menü butonu veya menü öğesi değilse, menü öğesinden "active" sınıfını kaldır
     if (
       !e.composedPath().includes(menuBtn) &&
       !e.composedPath().includes(navbar)
